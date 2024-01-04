f28f07f7c5f1730268e4170d13b0215b92cad07d
Caio Luiz Dos Santos <devcaiosantos@gmail.com>
2024-01-04 20:07:35 -0300

    Variável de ambiente

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.ts
+import { defineConfig, UserConfig,loadEnv } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default ({ mode }: { mode: string }): UserConfig => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+
+  return defineConfig({
+    plugins: [react()],
+
+    server: {
+      port: parseInt(process.env.VITE_PORT || '3000'), // Definindo um valor padrão caso VITE_PORT não esteja definido
+    },
+  });
+};
