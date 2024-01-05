7908b482e3459c5d879781b33206d26ef434c751
Caio Luiz Dos Santos <devcaiosantos@gmail.com>
2024-01-05 14:57:31 -0300

    Remoção React Router DOM

--- src/main.tsx
+++ src/main.tsx
@@ -1,23 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-//import App from './App.tsx'
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
-import Root from "./routes/root";
-import ErrorPage from './error-page';
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Root />,
-    errorElement: <ErrorPage />,
-  },
-]);
+import App from './App.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <App/>
   </React.StrictMode>,
 )
